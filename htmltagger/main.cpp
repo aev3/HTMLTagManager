8a7fcd9e99e248c12ebd75f804e16b5dbee52bf5
rtv3 <art@AWOLart.com>
2014-05-23 21:02:17 -0400

    Initial Commit

---
+++ htmltagger/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  htmltagger
+//
+//  Created by Arthur E. Vossberg III on 23-05-2014.
+//  Copyright (c) 2014 Art Vossberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
